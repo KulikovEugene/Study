48d844280e53a030aa8024aebd361834b03ea162
Eugene Kulikov <kulikoveugene090493@gmail.com>
2020-03-01 16:22:23 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Second commit
+          Third commit
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
