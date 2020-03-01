befba161e93535106a99edb5a3415f2a78edbf4a
Eugene Kulikov <kulikoveugene090493@gmail.com>
2020-03-01 16:14:47 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Second commit
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
